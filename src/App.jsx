8a89650c471f7081899eebcea3f434a50e3df1ad
benjalegree <149977722+benjalegree@users.noreply.github.com>
2026-02-12 08:33:33 +0100

    Add Companies route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Layout from "./layout/Layout"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
+import Companies from "./pages/Companies"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Login público */}
         <Route path="/" element={<Login />} />
 
+        {/* Dashboard / Overview */}
         <Route
           path="/dashboard"
           element={
@@ -17,6 +22,17 @@ export default function App() {
             </Layout>
           }
         />
+
+        {/* Companies */}
+        <Route
+          path="/companies"
+          element={
+            <Layout>
+              <Companies />
+            </Layout>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   )
