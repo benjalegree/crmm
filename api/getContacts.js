85a647a50428261af02b6d31d133d35fa58f1baa
benjalegree <149977722+benjalegree@users.noreply.github.com>
2026-02-12 09:24:18 +0100

    Update getContacts.js

--- api/getContacts.js
+++ api/getContacts.js
@@ -6,9 +6,16 @@ export default async function handler(req, res) {
     return res.status(401).json({ error: "Not authenticated" });
   }
 
+  const email = cookie
+    .split("session=")[1]
+    ?.split(";")[0]
+    ?.trim()
+    ?.toLowerCase();
+
   try {
+
     const response = await fetch(
-      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Contacts`,
+      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Contacts?filterByFormula={Responsible Email}='${email}'`,
       {
         headers: {
           Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`
@@ -17,10 +24,6 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
 
     return res.status(200).json(data);
 
